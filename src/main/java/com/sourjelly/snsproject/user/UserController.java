55313bc459a9c35ffde9f6cc6407895f3eb86633
mega202 <megaIT@megaIT>
2025-12-04 18:32:51 +0900

    [학원] - 로그인 화면 구성 완료 - 회원가입 화면 구성중

---
+++ src/main/java/com/sourjelly/snsproject/user/UserController.java
@@ -0,0 +1,16 @@
+package com.sourjelly.snsproject.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/user")
+@Controller
+public class UserController {
+
+
+    @GetMapping("/login")
+    public String login(){
+        return "user/login";
+    }
+}
---
+++ src/main/java/com/sourjelly/snsproject/user/UserRestController.java
@@ -0,0 +1,4 @@
+package com.sourjelly.snsproject.user;
+
+public class UserRestController {
+}
